70708b5fce9102b6f4571e716fb298ec2450a68f
19alexandr80 <usanka1980@gmail.com>
2023-03-18 20:29:35 +0200

    color

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ function onCoincidentCountry(params) {
   const tablo = params
     .map(
       param => `<div><img src="${param.flags.svg}" alt="Прапор" width="30">
-      <span style="color:red; font-size:26px;">  ${param.name.common}</span></div>`
+      <span style="color:#10174A; font-size:26px; text-shadow: 1px 1px 2px #12855D, 0 0 1em blue, 0 0 0.2em blue;">  ${param.name.common}</span></div>`
     )
     .join('');
   countryInfo.insertAdjacentHTML('beforeEnd', tablo);
@@ -73,9 +73,15 @@ function onCoincidentCountry(params) {
 function onDataCountry(params) {
   const bablo = `<img src="${params[0].flags.svg}" alt="Прапор" width="90">
   <h2>${params[0].name.official}</h2>
-  <p>capital: ${params[0].capital}</p>
-  <p>population: ${params[0].population}</p>
-  <p>languages: ${Object.values(params[0].languages).join(' , ')}</p>`;
+  <p><span style="color:#000085; font-weight: bold;">capital</span>: ${
+    params[0].capital
+  }</p>
+  <p><span style="color:#000085; font-weight: bold;">population</span>: ${
+    params[0].population
+  }</p>
+  <p><span style="color:#000085; font-weight: bold;">languages</span>: ${Object.values(
+    params[0].languages
+  ).join(' , ')}</p>`;
   countryInfo.insertAdjacentHTML('beforeEnd', bablo);
 }
 // Ukraine;
