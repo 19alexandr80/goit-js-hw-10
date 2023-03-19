97cd20f30db8791110c4848eca9f20fd999f5dff
19alexandr80 <usanka1980@gmail.com>
2023-03-19 06:33:30 +0200

    status

--- src/api-servise.js
+++ src/api-servise.js
@@ -2,6 +2,10 @@ function fetchCountry() {
   return fetch(
     'https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages'
   ).then(res => {
+    if (!res.ok) {
+      console.log(res.status);
+      throw new Error(res.status);
+    }
     return res.json();
   });
 }
